8d8b047c87aa5e6f556876be3337ae47b3375ca5
James Requenez <jrequenez@hotmail.com>
2023-08-31 09:07:58 -0500

    🚧  wip: troubleshooting test project deps

---
+++ BuildingMonitor/BuildingMonitor/Actors/TemperatureSensor.cs
@@ -0,0 +1,21 @@
+﻿using Akka.Actor;
+
+namespace BuildingMonitor.Actors
+{
+    public class TemperatureSensor : UntypedActor
+    {
+        public TemperatureSensor(string floorId, string sensorId)
+        {
+
+        }
+
+        protected override void OnReceive(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Props Props(string floorId, string sensorId) =>
+            Akka.Actor.Props.Create(() => new TemperatureSensor(floorId, sensorId));
+    }
+}
+
---
+++ BuildingMonitor/BuildingMonitor/Messages/ResponseMetadata.cs
@@ -0,0 +1,17 @@
+﻿namespace BuildingMonitor.Messages
+{
+	 public sealed class ResponseMetadata
+	{
+		public long RequestId { get; }
+		public string FloorId { get; }
+		public string SensorId { get; }
+
+		public ResponseMetadata(long requestId, string floorId, string sensorId)
+		{
+			RequestId = requestId;
+			FloorId = floorId;
+			SensorId = sensorId;
+		}
+	}
+}
+
