5dc22a16637b7856f39a4ba6104d812a4f969174
= <jrequenez@hotmail.com>
2023-08-31 09:35:38 -0500

    wip: update test project deps

--- BuildingMonitor/BuildingMonitor.Tests/TemperatureSensorShould.cs
+++ BuildingMonitor/BuildingMonitor.Tests/TemperatureSensorShould.cs
@@ -1,4 +1,4 @@
-﻿using Xunit;
+using Xunit;
 using Akka.Actor;
 using Akka.TestKit.Xunit2;
 using BuildingMonitor.Actors;
@@ -6,29 +6,29 @@ using BuildingMonitor.Messages;
 
 namespace BuildingMonitor.Tests
 {
-	public class TemperatureSensorShould : TestKit
-	{
-		[Fact]
-		public void InitializeSensorMetaData()
-		{
-			var probe = CreateTestProbe();
+    public class TemperatureSensorShould : TestKit
+    {
+        [Fact]
+        public void InitializeSensorMetaData()
+        {
+            var probe = CreateTestProbe();
 
-			var sensor = Sys.ActorOf(TemperatureSensor.Props("a", "1"));
+            var sensor = Sys.ActorOf(TemperatureSensor.Props("a", "1"));
 
-			sensor.Tell(new RequestMetadata(1), probe.Ref);
+            sensor.Tell(new RequestMetadata(1), probe.Ref);
 
-			var received = probe.ExpectMsg<ResponseMetadata>();
+            var received = probe.ExpectMsg<ResponseMetadata>();
 
-			Assert.Equal(1, received.RequestId);
-			Assert.Equal("a", received.FloorId);
-			Assert.Equal("1", received.SensorId);
-		}
+            Assert.Equal(1, received.RequestId);
+            Assert.Equal("a", received.FloorId);
+            Assert.Equal("1", received.SensorId);
+        }
 
-		[Fact]
-		public void StartWithNoTemperatureReading()
-		{
-			Assert.Equal(1, 1);
-		}
-	}
+        [Fact]
+        public void StartWithNoTemperatureReading()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
 }
 
