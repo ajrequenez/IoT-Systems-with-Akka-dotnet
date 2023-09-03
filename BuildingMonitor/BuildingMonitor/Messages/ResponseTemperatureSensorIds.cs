081f6e9ede25a41fafb18965f81bfd056200bf03
James Requenez <jrequenez@hotmail.com>
2023-09-02 23:24:06 -0500

    🐛  fix: ensure message immutability - ResponseTemperatureSensorIds msg

--- BuildingMonitor/BuildingMonitor/Messages/ResponseTemperatureSensorIds.cs
+++ BuildingMonitor/BuildingMonitor/Messages/ResponseTemperatureSensorIds.cs
@@ -1,11 +1,13 @@
+using System.Collections.Immutable;
+
 namespace BuildingMonitor.Messages
 {
     public sealed class ResponseTemperatureSensorIds
     {
         public long RequestId { get; }
-        public ISet<string> SensorIds { get; }
+        public IImmutableSet<string> SensorIds { get; }
 
-        public ResponseTemperatureSensorIds(long requestId, ISet<string> sensorIds)
+        public ResponseTemperatureSensorIds(long requestId, IImmutableSet<string> sensorIds)
         {
             RequestId = requestId;
             SensorIds = sensorIds;
