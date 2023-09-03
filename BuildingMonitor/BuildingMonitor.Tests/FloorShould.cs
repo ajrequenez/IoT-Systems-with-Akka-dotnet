64b43460700d40e9fceb5e1946eeb7721719cf17
James Requenez <jrequenez@hotmail.com>
2023-09-02 20:37:07 -0500

    ✅  feat: module 4.5 add floor actor and test - register new temp sensor - return exising sensor when registering same id

---
+++ BuildingMonitor/BuildingMonitor.Tests/FloorShould.cs
@@ -0,0 +1,44 @@
+﻿using Akka.TestKit.Xunit2;
+using BuildingMonitor.Actors;
+using BuildingMonitor.Messages;
+
+namespace BuildingMonitor.Tests
+{
+	public class FloorShould : TestKit
+	{
+		[Fact]
+		public void RegisterTemperatureSensorWhenDoesNotAlreadyExist()
+		{
+			var probe = CreateTestProbe();
+			var floor = Sys.ActorOf(Floor.Props("a"));
+
+			floor.Tell(new RequestRegisterTemperatureSensor(10, "a", "11"), probe.Ref);
+
+			var received = probe.ExpectMsg<ResponseRegisterTemperatureSensor>();
+
+			Assert.Equal(10, received.RequestId);
+
+            // Could check if sensor is repsonding
+			//var sensor = probe.LastSender;
+        }
+
+		[Fact]
+		public void ReturnExistingSensorWhenRegisteringSameSensor()
+		{
+			var probe = CreateTestProbe();
+			var floor = Sys.ActorOf(Floor.Props("b"));
+
+            floor.Tell(new RequestRegisterTemperatureSensor(21, "b", "11"), probe.Ref);
+
+            var received = probe.ExpectMsg<ResponseRegisterTemperatureSensor>();
+			var firstSensor = probe.LastSender;
+
+			// Try to register same sensor
+            floor.Tell(new RequestRegisterTemperatureSensor(22, "b", "11"), probe.Ref);
+            var receivedSecond = probe.ExpectMsg<ResponseRegisterTemperatureSensor>();
+            var secondSensor = probe.LastSender;
+
+			Assert.Equal(firstSensor, secondSensor);
+        }
+    }
+}
---
+++ BuildingMonitor/BuildingMonitor/Actors/Floor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Akka.Actor;
+using BuildingMonitor.Actors;
+using BuildingMonitor.Messages;
+
+namespace BuildingMonitor.Actors
+{
+	public class Floor : UntypedActor
+	{
+        private readonly string _floorId;
+        private Dictionary<string, IActorRef> _sensorIdToActorRefMap = new Dictionary<string, IActorRef>();
+
+		public Floor(string floorId)
+		{
+            _floorId= floorId;
+		}
+
+        protected override void OnReceive(object message)
+        {
+            switch (message)
+            {
+                case RequestRegisterTemperatureSensor m:
+                    if(_sensorIdToActorRefMap.TryGetValue(m.SensorId, out var existingActor))
+                    {
+                        // Return ref is existing
+                        existingActor.Forward(m);
+                    }
+                    else
+                    {
+                        // Create new temperature sensor and fwd registreaion message
+                        var newSensorActor = Context.ActorOf(
+                            TemperatureSensor.Props(m.FloorId, m.SensorId),
+                            $"temperature-sensor-{m.SensorId}");
+                        _sensorIdToActorRefMap.Add(m.SensorId, newSensorActor);
+                        newSensorActor.Forward(m);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public static Props Props(string floorId) => Akka.Actor.Props.Create(() => new Floor(floorId));
+    }
+}
+
