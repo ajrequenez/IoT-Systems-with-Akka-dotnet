1fbd46700e9e8d64451ce3f00de578446abf31d3
James Requenez <jrequenez@hotmail.com>
2023-09-04 22:37:09 -0500

    ✨  feat: module 7 add console host - completion of course

---
+++ BuildingMonitor/ConsoleAppHost/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Akka.Actor;
+using BuildingMonitor.Actors;
+using BuildingMonitor.Messages;
+
+namespace ConsoleAppHost
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting actor system...");
+
+            using (var system = ActorSystem.Create("building-iot-system"))
+            {
+                Console.WriteLine("     Floor Manager: Creating....");
+                IActorRef floorManager = system.ActorOf(Props.Create<FloorManager>(), "floor-manager");
+                Console.WriteLine("     Floor Manager: Created....");
+
+                await CreateSimulatedSensors(floorManager);
+
+                while (true)
+                {
+                    Console.WriteLine("Press enter to query, Q to quit");
+                    var cmd = Console.ReadLine();
+
+                    if (cmd?.ToUpperInvariant() == "Q")
+                    {
+                        Console.WriteLine("Shutting Down...");
+                        Environment.Exit(0);
+                    }
+                    await DisplayTemperatures(system);
+                }
+            }
+        }
+
+
+        private static async Task CreateSimulatedSensors(IActorRef floorManager)
+        {
+            for (int simulatedSensorId = 0; simulatedSensorId < 10; simulatedSensorId++)
+            {
+                Console.WriteLine($"     Sensor{simulatedSensorId}: Creating....");
+                var newSensor = new SimulatedSensor("basement", $"{simulatedSensorId}", floorManager);
+                await newSensor.Connect();
+                Console.WriteLine($"     Sensor{simulatedSensorId}: Connected....");
+
+                var simulateNoReadingYet = simulatedSensorId == 3;
+
+                if (!simulateNoReadingYet)
+                {
+                    newSensor.StartSendingSimulatedReadings();
+                }
+            }
+        }
+
+        private static async Task DisplayTemperatures(ActorSystem actorSystem)
+        {
+            var temps = await actorSystem.ActorSelection("akka://building-iot-system/user/floor-manager/floor-basement")
+                                    .Ask<ResponseAllTemperatures>(new RequestAllTemperatures(0));
+            Console.Clear();
+
+            foreach (var temp in temps.Temperatures)
+            {
+                Console.Write($"Sensor {temp.Key} {temp.Value.GetType().Name}");
+                if (temp.Value is TemperatureAvailable)
+                {
+                    Console.Write($" {((TemperatureAvailable)temp.Value).Temperature:00.00}");
+                }
+                Console.WriteLine("                   ");
+            }
+        }
+    }
+}
---
+++ BuildingMonitor/ConsoleAppHost/SimulatedSensor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Akka.Actor;
+using BuildingMonitor.Actors;
+using BuildingMonitor.Messages;
+
+namespace ConsoleAppHost
+{
+	public class SimulatedSensor
+	{
+		private IActorRef _floorManager;
+		private readonly Random _randomTemperatureGenerator;
+		private readonly string _floorId;
+		private readonly string _sensorId;
+		private IActorRef? _sensorReference;
+		private Timer? _timer;
+
+		public SimulatedSensor(string floorId, string sensorId, IActorRef floorManager)
+		{
+			_floorId = floorId;
+			_sensorId = sensorId;
+			_floorManager = floorManager;
+			_randomTemperatureGenerator = new Random(int.Parse(sensorId));
+		}
+
+		public async Task Connect()
+		{
+			var response = await _floorManager.Ask<ResponseRegisterTemperatureSensor>(
+				new RequestRegisterTemperatureSensor(1, _floorId, _sensorId));
+
+			_sensorReference = response.SensorRef;
+		}
+
+		public void StartSendingSimulatedReadings()
+		{
+			_timer = new Timer(SimulateUpdateTemperature, null, 0, 1000);
+		}
+
+		private void SimulateUpdateTemperature(object? sender)
+		{
+			var randomTemperature = _randomTemperatureGenerator.NextDouble()*100;
+
+			_sensorReference.Ask(new RequestUpdateTemperature(0, randomTemperature));
+
+		}
+	}
+}
+
